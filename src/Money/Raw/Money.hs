7c78044a116c4ef11e7026662344d58eba731a7a
Alexander Granin <graninas@gmail.com>
2020-10-03 02:02:24 +0700

    TSMoney opt1

--- src/Money/Raw/Money.hs
+++ src/Money/Raw/Money.hs
@@ -14,9 +14,14 @@ module Money.Raw.Money where
 data Money = Money Int
   deriving (Show, Read, Eq, Ord)
 
+mkMoney :: Int -> Money
+mkMoney = Money
 
 add :: Money -> Money -> Money
 add (Money m1) (Money m2) = Money $ m1 + m2
+
+mul :: Money -> Int -> Money
+mul (Money m1) v = Money $ m1 * v
 
 zero :: Money
 zero = Money 0
--- src/Money/TypeSafe/Money.hs
+++ src/Money/TypeSafe/Money.hs
@@ -21,3 +21,19 @@ class Currency cur
 
 mkTSMoney :: Currency cur => RM.Money -> TSMoney cur
 mkTSMoney = TSMoney
+
+
+add :: Currency cur => TSMoney cur -> TSMoney cur -> TSMoney cur
+add (TSMoney m1) (TSMoney m2) = TSMoney $ RM.add m1 m2
+
+convWithRate
+  :: forall cur1 cur2
+   . Currency cur1
+  => Currency cur2
+  => TSMoney cur1
+  -> Int
+  -> TSMoney cur2
+convWithRate (TSMoney m1) v = mkTSMoney $ RM.mul m1 v
+
+zero :: Currency cur => TSMoney cur
+zero = mkTSMoney RM.zero
