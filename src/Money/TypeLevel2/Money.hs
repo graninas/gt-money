ee0905b0d199591ed9e200c3381cd60297a860e6
Alexander Granin <graninas@gmail.com>
2020-10-16 23:52:59 +0700

    Simplifications.

--- src/Money/TypeLevel2/Money.hs
+++ src/Money/TypeLevel2/Money.hs
@@ -17,24 +17,30 @@ import           GHC.TypeLits (Symbol)
 
 data Car (name :: Symbol) (engine :: EngineTag x)
 
-data EngineTag x = EngineTag
+data EngineTag a
+
+
 
 type family Engine (a :: *) :: EngineTag a
 
 -- Implementation
 
-class Eval tag payload res where
-  eval :: tag -> Proxy payload -> IO res
+class Eval payload res where
+  eval :: Proxy payload -> IO res
 
 
-instance Eval () FusionMkI () where
-  eval _ _ = undefined
+instance Eval FusionMkI () where
+  eval _ = undefined
 
-instance Eval () BrokenEngine () where
-  eval _ _ = undefined
+instance Eval BrokenEngine () where
+  eval _ = undefined
 
-instance (b ~ Engine a, Eval () a ()) => Eval () b () where
-  eval _ _ = eval () (Proxy :: Proxy a)
+instance (b ~ Engine a, Eval a ()) => Eval b () where
+  eval _ = eval (Proxy :: Proxy a)
+
+
+instance Eval engine () => Eval (Car name engine) () where
+  eval _ = eval (Proxy :: Proxy engine)
 
 
 
@@ -45,13 +51,14 @@ data FusionMkI
 data BrokenEngine
 
 type MyCar1 = Car "A" (Engine FusionMkI)
-type MyCar2 = Car "A" (Engine BrokenEngine)
+type MyCar2 = Car "B" (Engine BrokenEngine)
 
 
 runner :: IO ()
 runner = do
-  () <- eval () (Proxy :: Proxy (Engine FusionMkI))
-  () <- eval () (Proxy :: Proxy (Engine BrokenEngine))
+  () <- eval (Proxy :: Proxy (Engine FusionMkI))
+  () <- eval (Proxy :: Proxy (Engine BrokenEngine))
+  () <- eval (Proxy :: Proxy MyCar1)
   pure ()
 
 
